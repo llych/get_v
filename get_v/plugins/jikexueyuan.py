2229b00ee00354af05e51e25ec1534edec00d14a
llych <llych@126.com>
2023-01-23 22:46:26 +0800

    Update jikexueyuan.py

--- get_v/plugins/jikexueyuan.py
+++ get_v/plugins/jikexueyuan.py
@@ -2,8 +2,8 @@
 '''
 Created on Jun 28, 2015
 
-@author: ÀèÓî³õ
-@mail: llych#126.com
+@author: é»**
+@mail: l**#126.com
 '''
 
 import requests
@@ -11,11 +11,11 @@ import re
 import sys
 
 description = '''
-¼«¿ÍÑ§Ôº (µ°ÌÛ,ĞèÒªvipÕÊºÅ,×¢²á¸öĞ¡ºÅÌåÑévip
-         µÇÂ¼ºó°Ñcookie ÌîÈë²å¼şÀï get_v\plugins\jikexueyuan.py)
-ÀıÈç  --url=http://www.jikexueyuan.com/course/1439.html'''
+æå®¢å­¦é™¢ (è›‹ç–¼,éœ€è¦vipå¸å·,æ³¨å†Œä¸ªå°å·ä½“éªŒvip
+         ç™»å½•åæŠŠcookie å¡«å…¥æ’ä»¶é‡Œ get_v\plugins\jikexueyuan.py)
+ä¾‹å¦‚  --url=http://www.jikexueyuan.com/course/1439.html'''
 
-#----------------ÌîÈëÒÔÏÂcookie ä¯ÀÀÆ÷Àï»ñÈ¡
+#----------------å¡«å…¥ä»¥ä¸‹cookie æµè§ˆå™¨é‡Œè·å–
 cookie='stat_uuid=1434994598214629244303; undefined=; channel=invite_100w_sharebutton_direct1; stat_ssid=1436397140035; stat_isNew=0; uname=jike_tvzmnh71964; uid=3687067; code=7K3IN6; authcode=8ef4VyRgO0VVAIp0VCRVIZ%2BGSm6ovB3ka3iiH0NwGCzkqGH0Qq3BvW28db08JaX900SIM1OhOHpwpBcwALGhHOjamhd2hg9nXw5iGq8ptkFMXORACDIISUY6%2FdqI%2BhlLFVOSow; QINGCLOUDELB=7e36c8b37b8339126ed93010ae808701d562b81daa2a899c46d3a1e304c7eb2b|VZFwr|VZFwm; Hm_lvt_f3c68d41bda15331608595c98e9c3915=1435468507,1435507848,1435530201,1435594856; Hm_lpvt_f3c68d41bda15331608595c98e9c3915=1435594873; MECHAT_LVTime=1435594873477; connect.sid=s%3AikXrRmtHErUDkQ9Ut0ms96Fr3OMPQVl-.Go9eAQtdT3OiOwM%2BBnhW4%2BDzJQHZx%2BTYpuUV4Qvipkg; _ga=GA1.2.807642645.1434994597; _gat=1;'
 def handle(url):
     session = requests.session()
@@ -28,11 +28,11 @@ def handle(url):
             videoUrl = re.findall('<source src="(http://.*?.jikexueyuan.com/.*?)" type="video/.*?" />',html)[0]
             return videoUrl
         except Exception, e:
-            print url,'ÊÓÆµÎ´ÕÒµ½'
+            print url,'è§†é¢‘æœªæ‰¾åˆ°'
             
     html = session.get(url).content
     videos = re.findall('<span class="sm-icon ">.*?<a href="(http://.*?)" jktag=".*?">(.*?)</a>',html,re.S)
-    print 'ÊÓÆµ ¹² %s ¸ö'%len(videos)
+    print 'è§†é¢‘ å…± %s ä¸ª'%len(videos)
     for url,fileName in videos:
         fileName=fileName.decode('UTF-8').encode('GBK')
         yield getVideo(url),fileName+'.mp4'
